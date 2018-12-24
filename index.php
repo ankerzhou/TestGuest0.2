a37a40d249da90cafc1dd0ac45f830e30989286f
ankerzhou <zgg.lu@aliyun.com>
2018-12-25 00:10:05 +0800

    TestGuest第二个版本

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+/**
+* ==============================================
+* @date: 2018年12月12日
+* @author: Anker_Zhou
+* @version: 1.0
+* ==============================================
+*/
+
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+<title>Your Title</title>
+<link rel="stylesheet" type="text/css" href="Styles/1/basic.css"></link>
+<link rel="stylesheet" type="text/css" href="Styles/1/index.css"></link>
+</head>
+<body>
+<div id="header" >
+<h1><a href="index.php">朴成web俱乐部多用户留言系统</a></h1>
+<ul>
+<li>首页</li>
+<li>登录</li>
+<li>注册</li>
+<li>个人中心</li>
+<li>风格</li>
+<li>管理</li>
+<li>退出</li>
+</ul>
+</div>
+<div id="list">
+<h2>帖子列表</h2>
+</div>
+<div id="user">
+<h2>新进会员</h2>
+</div>
+<div id="pics">
+<h2>最新图片</h2>
+</div>
+<div id="footer">
+<p>版权所有 翻版必究</p>
+<p>本程序由<span>朴成web俱乐部</span>提供 源代码可以任意修改或发布(c) yc60.com</p>
+</div>
+</body>
+</html>
